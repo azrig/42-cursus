abd0fd8eb9e1ee27abf25d5386463308b7131648
Alaa Zrig <alaazrig0322@gmail.com>
2025-01-05 22:47:30 +0100

    push

--- printf/ft_puthexa.c
+++ printf/ft_puthexa.c
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_puthexa.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azrig <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: azrig <azrig@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 14:56:08 by azrig             #+#    #+#             */
-/*   Updated: 2025/01/05 21:53:17 by azrig            ###   ########.fr       */
+/*   Updated: 2025/01/05 22:44:31 by azrig            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	ft_puthexa(unsigned int n, char format)
 {
 	char	*alpha;
+	int		count;
 
-	if (format == 'x')
-		alpha = "0123456789abcdef";
+	alpha = "0123456789abcdef";
 	if (format == 'X')
 		alpha = "0123456789ABCDEF";
-	return (ft_base16(n, alpha));
+	count = 0;
+	if (n > 15)
+	{
+		count += ft_puthexa(n / 16, format);
+		count += ft_puthexa(n % 16, format);
+	}
+	else
+		count += ft_putchar(alpha[n]);
+	return (count);
 }

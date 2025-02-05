d3603064b043c42a69aba651bceda753aa92b11a
Alaa Zrig <alaazrig0322@gmail.com>
2025-02-05 03:08:16 +0100

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,38 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azrig <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: azrig <azrig@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:27:01 by azrig             #+#    #+#             */
-/*   Updated: 2025/02/01 22:38:51 by azrig            ###   ########.fr       */
+/*   Updated: 2025/02/05 03:07:36 by azrig            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *check_stash(char **stash)
+static char	*check_stash(char **stash)
 {
-    char *line;
-    char *temp;
-    char *nl;
+	char	*line;
+	char	*temp;
+	char	*nl;
 
-    if (!*stash)
-        return (NULL);
-    nl = nl_pos(*stash);
-    if (!nl)
-    {
-        line = ft_strdup(*stash);
-        free(*stash);
-        *stash = NULL;
-    }
-    else
-    {
-        line = ft_substr(*stash, 0, nl - *stash + 1);
-        temp = ft_strdup(nl + 1);
-        free(*stash);
-        *stash = temp;
-    }
-    return (line);
+	if (!*stash)
+		return (NULL);
+	nl = nl_pos(*stash);
+	if (!nl)
+	{
+		line = ft_strdup(*stash);
+		free(*stash);
+		*stash = NULL;
+	}
+	else
+	{
+		line = ft_substr(*stash, 0, nl - *stash + 1);
+		temp = ft_strdup(nl + 1);
+		free(*stash);
+		*stash = temp;
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
